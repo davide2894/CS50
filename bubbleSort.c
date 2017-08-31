52a8cc391ce301a11097f605dbb4c9fa2f86cec6
Davide <davide.iaiunese@gmail.com>
2017-08-31 12:44:17 +0200

    Add bubble sort example in c#

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
+//swap function declaration
 void swap(int *a, int *b);
 
 int main(void)
@@ -8,30 +9,27 @@ int main(void)
     int values[] = {3,5,2,1,4};
     int swap_counter = -1;
 
-    printf("array: [%i,%i,%i,%i,%i]\n", values[0],values[1], values[2], values[3], values[4]);
-
-    while(swap_counter != 0)
+    while(swap_counter != 0) //the array is sorted when swap_counter is 0
     {
+        //reset swap counter to 0
         swap_counter = 0;
-        printf("\n");
-        printf("reset swap_counter to %i\n", swap_counter);
 
         for(int i=0; i<4; i++)
         {
             if(values[i]>values[i+1])
             {
+                //swap function call
                 swap(&values[i], &values[i+1]);
-                printf("pairing %i and %i\n", values[i], values[i+1]);
-                printf("swapped %i for %i\n", values[i], values[i+1]);
-                printf("array: [%i,%i,%i,%i,%i]\n", values[0],values[1], values[2], values[3], values[4]);
+
+                //increment swap_counter for each swap
                 swap_counter++;
-                printf("swap_counter: %i\n", swap_counter);
             }
         }
     }
     printf("sorted array: [%i,%i,%i,%i,%i]\n", values[0],values[1], values[2], values[3], values[4]);
 }
 
+//swap function definition
 void swap(int* a, int* b)
 {
     int tmp = *a;
