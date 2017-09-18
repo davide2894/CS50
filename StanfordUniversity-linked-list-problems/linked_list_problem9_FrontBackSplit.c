97d6bf5a1f50306e70cfc7cf961534f18bb66613
Davide <davide.iaiunese@gmail.com>
2017-09-18 19:43:43 +0200

    Update linked_list_problem9_FrontBackSplit.c

--- StanfordUniversity-linked-list-problems/linked_list_problem9_FrontBackSplit.c
+++ StanfordUniversity-linked-list-problems/linked_list_problem9_FrontBackSplit.c
@@ -9,8 +9,6 @@
 */
 
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,6 +16,9 @@
 
 int main(void)
 {
+    node* a = malloc(sizeof(node));
+    node* b = malloc(sizeof(node));
+
     //create list with 1 node, split it
     node* head0 = malloc(sizeof(node));
     head0->data = 2;
@@ -26,38 +27,7 @@ int main(void)
     append(head0, 5);
     append(head0, 7);
     append(head0, 11);
-    FrontBackSplit(head0);                       // A list: 2 3 5 | B list: 7 11
 
 
-
-    //create list with 1 node, split it
-    node* head1 = malloc(sizeof(node));
-    head1->data = 1;
-    head1->next = NULL;
-    createList(head1, 1);
-    FrontBackSplit(head1);                       //splt not possible: only one node is in the list
-
-
-    //create list with 2 nodes, split it
-    node* head2 = malloc(sizeof(node));
-    head2->data = 1;
-    head2->next = NULL;
-    createList(head2, 2);
-    FrontBackSplit(head2);                      // A list: 1 | B list: 2
-
-
-    //create list with 3 nodes, split it
-    node* head3 = malloc(sizeof(node));
-    head3->data = 1;
-    head3->next = NULL;
-    createList(head3, 3);
-    FrontBackSplit(head3);                      // A list: 1 2 | B list: 3
-
-
-    //create list with 4 nodes, split it
-    node* head4 = malloc(sizeof(node));
-    head4->data = 1;
-    head4->next = NULL;
-    createList(head4, 4);
-    FrontBackSplit(head4);                      // A list: 1 2 3 | B list: 4
+    FrontBackSplit(head0, a, b);                       // A list: 2 3 5 | B list: 7 11
 }
