434ab1442d382aa6ac2c72484c53346c24144bbb
Davide <davide.iaiunese@gmail.com>
2017-09-19 19:37:02 +0200

    Fixed linked_list_problem2_GetNth.c

--- StanfordUniversity-linked-list-problems/linked_list_problem2_GetNth().c
+++ StanfordUniversity-linked-list-problems/linked_list_problem2_GetNth().c
@@ -1,4 +1,6 @@
 /*
+    Stanford University - Computer Science - Linked list exercises
+
     Create a GetNth function that takes a linked list
     and an integer index and returns the value
     stored in the node at that index position
@@ -8,55 +10,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "linked.h"
+#include "list.h"
 
+/*
 
-int GetNth(node* head, int index_to_find)
+    Function used from list.h
+
+int GetNth(node* head, int index)
 {
-    //create node to traverse, then assign head data and link to it
     node* current = head;
-
-    //set count to 0
     int count = 0;
+    int value = 0;
 
-    //traverse list until the end
     while(current != NULL)
     {
-        if(count == index_to_find)
+        if(count == index)
         {
-            printf("data value %i is at index %i\n", current->data, index_to_find);
+            value = current->data;
             break;
         }
         else
         {
-            //move current to the next node
             current = current->next;
-
-            //update counr
-            count++;
+            value++;
         }
     }
-
-    //assert fail if we get here, because it would mean
-    //the index we are looking for is not in the list
-    if(current==NULL && index_to_find != count)
-    {
-        printf("node of index %i is not in the list\n", index_to_find);
-        return 1;
-    }
-    else
-        return 0;
+    return value;
 }
+
+
+*/
 
 
 int main(void)
 {
-    node* head = malloc(sizeof(node));
-    head->data = 1;
-    head->next = NULL;
+    node* myList = BuildOneTwoThree();      //Build 1 2 3
+    int value = GetNth(myList, 2);          //Gets the value at index 2
+                                            //of the list
 
-    createList(head, 5);
-    printList(head);
-
-    GetNth(head, 4);
+    printf("%i\n", value);                  //which is 3
 }
