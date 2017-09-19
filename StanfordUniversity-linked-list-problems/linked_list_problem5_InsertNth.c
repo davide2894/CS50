9dac97913cfad3ce6d62b196a4e364b9a19f45ac
Davide <davide.iaiunese@gmail.com>
2017-09-19 19:41:58 +0200

    Update linked_list_problem5_InsertNth.c

--- StanfordUniversity-linked-list-problems/linked_list_problem5_InsertNth.c
+++ StanfordUniversity-linked-list-problems/linked_list_problem5_InsertNth.c
@@ -13,97 +13,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "linked.h"
+#include "list.h"
 
 
-void InsertNth(node** head, int index, int data_to_insert)
+/*
+
+Functions used from list.h
+
+void InsertNth(node** head, int index, int data)
 {
-        //point current to head
-        node* current = *head;
+    node* current = *head;
 
-        node* prevNode = current;
-
-        //initialize position counter
-        int count = -1;
-
-
-
-        while(current)
+    if(index == 0)
+        Push(head, data);
+    else
+    {
+        for(int i = 0; i < index-1; i++)
         {
-                //if goal index is head node but it is already occupied
-                if(index == 0 && count == 0 && current != NULL)
-                {
-                        printf("position of index %i found!\n", index);
-
-                        //create new node and set it to current position
-                        node* new_node = malloc(sizeof(node));
-                        new_node->data = data_to_insert;
-                        new_node->next = *head;
-
-                        //make new node the new head
-                        *head = new_node;
-
-                        //exit loop
-                        break;
-                }
-                //if goal index is not head node and that positon is already occupied
-                else if(count == index && count != 0 && index != 0 && current != NULL)
-                {
-                        //give new node current position and argument data
-                        node* new_node = malloc(sizeof(node));
-                        new_node->data = data_to_insert;
-
-                        //link previous node to new node
-                        prevNode->next = new_node;
-
-                        new_node->next = current;
-
-                        //move current to next node
-                        current = current->next;
-
-                        //exit loop
-                        break;
-                }
-                //if goal index is last node
-                else if(current == NULL)
-                {
-                        node* new_node = malloc(sizeof(node));
-                        new_node->data = data_to_insert;
-                        new_node->next = NULL;
-
-                        current->next = new_node;
-                }
-                else
-                {
-                        //give previous node the current position
-                        prevNode = current;
-
-                        //move to the next node
-                        current = current->next;
-
-                        //update position counter
-                        count++;
-
-                }
+            current = current->next; //reach desired index position
         }
-
-        //if index is greater than available indexes of the list
-        if(index > count)
-                printf("given index %i exceeds last linked list node at index %i\n", index, count);
-
+        Push(&(current->next), data); //push in front of the next node
+                                      //i.e. at the desired index
+    }
 }
+
+
+void PrintList(node* head)
+{
+    node* current = head;
+
+    while(current != NULL)
+    {
+        printf("%i ", current->data);
+        current = current->next;
+    }
+
+    if(head == NULL)
+    {
+        printf("empty\n");
+    }
+
+
+
+*/
+
+
 
 
 
 int main(void)
 {
-        node* head = malloc(sizeof(node));
-        head->data = 1;
-        head->next = NULL;
+        node* head = NULL;  //start with empty list
+        InsertNth(&head, 0, 1);
+        InsertNth(&head, 1, 3);
+        InsertNth(&head, 1, 5);
+        PrintList(head);        //1 3 5
 
-        createList(head, 3);
-        printList(head);
 
-        InsertNth(&head, 3, 4);
-        printList(head);
 }
