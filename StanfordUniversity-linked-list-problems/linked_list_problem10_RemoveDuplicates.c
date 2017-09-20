d2e28a4daa9dab9cdc3f8f818271b375f45d92e8
Davide <davide.iaiunese@gmail.com>
2017-09-20 11:41:10 +0200

    Fixed linked_list_problem10_RemoveDuplicates

--- StanfordUniversity-linked-list-problems/linked_list_problem10_RemoveDuplicates.c
+++ StanfordUniversity-linked-list-problems/linked_list_problem10_RemoveDuplicates.c
@@ -4,38 +4,124 @@
 
     Write a RemoveDuplicates() function which takes a list sorted in increasing order and
     deletes any duplicate nodes from the list. Ideally, the list should only be traversed once
-
 */
 
+
+/*
+
+Functions used from list.h
+
+void RemoveDuplicates(node* head)
+{
+    //get list length
+    int len = Length(head);
+
+    if(len == 1 || head == NULL)
+    {
+        printf("Nothing to remove\n");
+    }
+
+    node* current = head;
+
+    //iterate until next node is not empty otherwise there would be nothing to compare to
+    while(current->next != NULL)
+    {
+        //compare current and next node
+        if(current->data == current->next->data)
+        {
+            node* nextNext = current->next->next;
+            free(current->next);                    //delete duplicate node
+            current->next = nextNext;               //skip deleted position
+        }
+        else
+        {
+            //if there is no deletion, advance to next node
+            current = current->next;
+        }
+    }
+}
+
+
+void InsertNth(node** head, int index, int data)
+{
+    node* current = *head;
+
+    if(index == 0)
+        Push(head, data);
+    else
+    {
+        for(int i = 0; i < index-1; i++)
+        {
+            current = current->next; //reach desired index position
+        }
+        Push(&(current->next), data); //push in front of the next node
+                                      //i.e. at the desired index
+    }
+}
+
+
+void PrintList(node* head)
+{
+    if(head == NULL)
+    {
+        printf("empty\n");
+    }
+    else
+    {
+        node* current = head;
+
+        while(current != NULL)
+        {
+            printf("%i ", current->data);
+            current = current->next;
+        }
+        printf("\n");
+    }
+}
+
+
+
+node* BuildOneTwoThree()
+{
+    node* head = malloc(sizeof(node));
+    node* second = malloc(sizeof(node));
+    node* third = malloc(sizeof(node));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    //head points to the list
+    return head;
+}
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "linked.h"
+#include "list.h"
 
 int main(void)
 {
-    node* head = malloc(sizeof(node));
-    head->data = 1;
-    head->next = NULL;
-    printList(head);                    //1 2 2 3 3
-    append(head, 2);
-    append(head, 2);
-    append(head, 3);
-    append(head, 3);
+    node* head = BuildOneTwoThree();
+    //at index 2 insert 3
+    InsertNth(&head, 3, 3);
+    //at index 3 insert 4
+    InsertNth(&head, 4, 4);
+    //at index 4 insert 7
+    InsertNth(&head, 5, 7);
 
-    RemoveDuplicates(head);             //1 2 2 3 3
+    printf("List: ");
+    PrintList(head);            // 1 2 3 3 4 7
 
+    printf("Deleting duplicates...\n");
+    RemoveDuplicates(head);
+    printf("Now list is: ");
+    PrintList(head);            // 1 2 3 4 7
 
-    printf("\n");
-
-    //case with no duplicates
-    node* head2 = malloc(sizeof(node));
-    head2->data = 1;
-    head2->next = NULL;
-
-    RemoveDuplicates(head2);             //No duplicate to remove
-                                         //list: 1
-
-    return 0;
 }
